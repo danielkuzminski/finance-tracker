dd6fb94972db26800146725240a2192a49bd86a8
danielkuzminski <dankuzmina@gmail.com>
2022-10-30 04:10:55 +0100

    git add signup and login pages

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react'
+
+//styles
 import './Login.css'
 
-import React from 'react'
-
 export default function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      <p>login</p>
-    </div>
+    <form className='submit-form' onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <label>
+        <span>your email: </span>
+        <input 
+          type="email" 
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>your password:</span>
+        <input 
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+          value={password}
+           />
+      </label>
+      <button className='submit-btn' >submit</button>
+    </form>
   )
 }
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,11 +1,53 @@
-import './Signup.css'
+import { useState } from 'react';
 
-import React from 'react'
+//styles
+import './Signup.css';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <p>signup</p>
-    </div>
-  )
+    <form onSubmit={handleSubmit} className='submit-form'>
+      <h1>signup</h1>
+      <label>
+        <span>your email: </span>
+        <input
+          type='email'
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>your password:</span>
+        <input
+          type='password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>repeat your password:</span>
+        <input
+          type='password'
+          onChange={(e) => {
+            setPassword2(e.target.value);
+          }}
+          value={password2}
+        />
+      </label>
+      <button className='submit-btn'>submit</button>
+    </form>
+  );
 }
