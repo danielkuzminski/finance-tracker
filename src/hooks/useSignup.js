e53df97c2f660a4d71dabb8802c2821335e89029
danielkuzminski <dankuzmina@gmail.com>
2022-11-15 18:25:44 +0100

    git work on useSignup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -5,17 +5,35 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 export const useSignup = () => {
   const [error, setError] = useState(null);
 
-  const signup = (email, password, displayName) => {
+  const signup = async (email, password, displayName) => {
     setError(null);
 
-    createUserWithEmailAndPassword(auth, email, password, displayName)
-      .then((res) => {
-        console.log('user signed in', email, password, displayName);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  }
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res.user);
 
+      if (!res) {
+        throw new Error('could not signup');
+      }
+
+      await res.user.updateProfile({ displayName });
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+    }
+
+    // const signup = (email, password, displayName) => {
+    //   setError(null);
+
+    //   createUserWithEmailAndPassword(auth, email, password, displayName)
+    //     .then((res) => {
+    //       console.log('user signed in', email, password, displayName);
+    //     })
+    //     .catch((err) => {
+    //       setError(err.message);
+    //     });
+    // }
+
+  };
   return { error, signup };
 };
